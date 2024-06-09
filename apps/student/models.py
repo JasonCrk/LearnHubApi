64ff86970602eaf31ca516f9955d0962640bb241
JasonCrk <emerzonkolki@gmail.com>
2024-06-08 23:32:53 -0500

    feat(student): Add is_active field in Student model

--- apps/student/models.py
+++ apps/student/models.py
@@ -7,6 +7,7 @@ from apps.user.models import UserAccount
 class Student(models.Model):
     user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
     class_room = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.user.first_name
