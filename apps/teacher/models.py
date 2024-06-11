6a34c7d4679597c54469bdfcf9602250cf602e8e
JasonCrk <emerzonkolki@gmail.com>
2024-06-10 23:56:33 -0500

    feat(teacher/models.py): Add model manager

--- apps/teacher/models.py
+++ apps/teacher/models.py
@@ -4,10 +4,18 @@ from apps.classroom.models import ClassRoom
 from apps.user.models import UserAccount
 
 
+class TeacherManager(models.Manager):
+    def create_owner_teacher(self, user, classroom):
+        return self.create(user=user, class_room=classroom, owner=True)
+
+
 class Teacher(models.Model):
     user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
     class_room = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
     owner = models.BooleanField(default=False)
 
+    objects = TeacherManager()
+
     def __str__(self) -> str:
         return self.user.firt_name
+
