9463b801f255d4374342b68b93cd2cfb59b2f065
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2024-06-14 16:08:24 -0500

    feat(teacher): Invitation model and migrations

--- apps/teacher/models.py
+++ apps/teacher/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from apps.classroom.models import ClassRoom
 from apps.user.models import UserAccount
 
+from .fields import generate_code
+
+import uuid
+
 
 class TeacherManager(models.Manager):
     def create_owner_teacher(self, user, classroom):
@@ -18,4 +22,21 @@ class Teacher(models.Model):
 
     def __str__(self) -> str:
         return self.user.firt_name
+
+
+class Invitation(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    sub_code = models.CharField(
+        verbose_name='Sub code',
+        max_length=9,
+        default=generate_code,
+        unique=True
+    )
+    accept_code = models.UUIDField(
+        verbose_name='Accept code',
+        default=uuid.uuid4,
+        unique=True
+    )
 
