fd2526cbba7ee427de2c63f206d54ba14e7c0120
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2024-06-15 03:54:59 -0500

    feat: Switch activation join classroom endpoint

--- apps/classroom/urls.py
+++ apps/classroom/urls.py
@@ -2,11 +2,25 @@ from django.urls import path
 
 from apps.classroom.views import (
     create_classroom_view,
+    switch_activation_join_classroom_view,
     join_classroom_view
 )
 
 
 urlpatterns = [
-    path('', create_classroom_view.create_classroom, name='create_classroom'),
-    path('join/<uuid:access_code>', join_classroom_view.join_classroom, name='join_classroom')
+    path(
+        '',
+        create_classroom_view.create_classroom,
+        name='create_classroom'
+    ),
+    path(
+        'join/<uuid:access_code>/',
+        join_classroom_view.join_classroom,
+        name='join_classroom'
+    ),
+    path(
+        '<int:classroom_id>/join/activation/',
+        switch_activation_join_classroom_view.switch_activation_join_classroom,
+        name='switch_activation_join_classroom'
+    )
 ]
---
+++ apps/classroom/views/switch_activation_join_classroom_view.py
@@ -0,0 +1,45 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+
+import uuid
+
+from apps.classroom.models import ClassRoom
+from apps.teacher.models import Teacher
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def switch_activation_join_classroom(request: Request, classroom_id: int):
+    try:
+        classroom = ClassRoom.objects.get(pk=classroom_id)
+    except ClassRoom.DoesNotExist:
+        return Response({
+            'message': 'The classroom does not exist'
+        }, status=status.HTTP_404_NOT_FOUND)
+
+    if not Teacher.objects.filter(
+        user=request.user,
+        class_room=classroom,
+        owner=True
+    ).exist():
+        return Response({
+            'message': 'You must be one of owners of the classroom'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    if classroom.available_join:
+        classroom.available_join = False
+        classroom.save()
+    else:
+        classroom.available_join = True
+        classroom.access_code = uuid.uuid4()
+        classroom.save()
+
+    return Response({
+        'message': f'The ability to join the classroom has been {
+            'activated' if classroom.available_join else 'disabled'
+        }'
+    }, status=status.HTTP_200_OK)
+
