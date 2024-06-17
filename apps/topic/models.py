2a3193859cbdd2baa0ba183c6f2ae39105600762
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2024-06-17 00:22:08 -0500

    feat(topic): Add classroom foreign key field

--- apps/topic/models.py
+++ apps/topic/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Topic(models.Model):
     name = models.CharField(verbose_name='Name', max_length=100, unique=True)
+    classroom = models.ForeignKey('classroom.Classroom', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
