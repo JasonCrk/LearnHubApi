b705cd6f245a77cff789db349582407abe702ba2
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2024-06-19 15:26:23 -0500

    feat(classroom): Assign teacher as classroom owner endpoint

--- apps/classroom/urls.py
+++ apps/classroom/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from apps.classroom.views import (
+    assign_teacher_as_classroom_owner_view,
     create_classroom_view,
     refresh_classroom_access_code_view,
     switch_activation_join_classroom_view,
@@ -28,5 +29,10 @@ urlpatterns = [
         '<int:classroom_id>/refresh-access/',
         refresh_classroom_access_code_view.refresh_classroom_access_code,
         name='refresh_classroom_access_code'
+    ),
+    path(
+        '<int:classroom_id>/assign_owner/<int:teacher_id>/',
+        assign_teacher_as_classroom_owner_view.assign_teacher_as_classroom_owner,
+        name='assign_teacher_as_classroom_owner'
     )
 ]
